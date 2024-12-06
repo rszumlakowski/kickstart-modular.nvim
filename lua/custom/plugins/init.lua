ff2bc8ebafb8188544b096b7da8c48e20eef0b0c
Rob Szumlakowski <rszumlakowsk@vmware.com>
2024-12-06 13:30:25 -0500

    Added multi cursors plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -61,4 +61,21 @@ return {
       require('refactoring').setup()
     end,
   },
+  {
+    'smoka7/multicursors.nvim',
+    event = 'VeryLazy',
+    dependencies = {
+      'nvimtools/hydra.nvim',
+    },
+    opts = {},
+    cmd = { 'MCstart', 'MCvisual', 'MCclear', 'MCpattern', 'MCvisualPattern', 'MCunderCursor' },
+    keys = {
+      {
+        mode = { 'v', 'n' },
+        '<Leader>m',
+        '<cmd>MCstart<cr>',
+        desc = 'Create a selection for selected text or word under the cursor',
+      },
+    },
+  },
 }
